478e0cc5174545d235a2c6367ff18b384b40672f
HeyStenson <h.n.stenson@gmail.com>
2015-08-15 11:26:15 -0700

    changing tactics

--- js/racing.js
+++ js/racing.js
@@ -1,13 +1,22 @@
+// $(document).ready(function(){
+// 	$(document).on('keypress', function onKeypress(event){
+// 		var notActive = $('div.road').hasClass('inactive') === true;
+// 		if (notActive){
+// 			$('div.road').next('.road').addClass('active');
+// 		} else {
+// 			$('div.road').addClass('inactive');
+// 			$('div.road').removeClass('active');
+			
+// 		}
+// 	});
+// })
+
 $(document).ready(function(){
 	$(document).on('keypress', function onKeypress(event){
-		var notActive = $('div.road').hasClass('active') === false;
-		if (notActive){
-			$('div.road').next('.road').addClass('active');
-		} else {
-			$('div.road').removeClass('active');
+		
+		if(event.which === 97){
+			console.log("okay");
 		}
 	});
 })
-
-
 
