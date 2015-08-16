9b523f71f13b9056873eedeca809d2b8c995dbe9
HeyStenson <h.n.stenson@gmail.com>
2015-08-16 10:34:43 -0700

    Got player 2 working

--- js/racing.js
+++ js/racing.js
@@ -1,28 +1,23 @@
-// $(document).ready(function(){
-// 	$(document).on('keypress', function onKeypress(event){
-// 		var notActive = $('div.road').hasClass('inactive') === true;
-// 		if (notActive){
-// 			$('div.road').next('.road').addClass('active');
-// 		} else {
-// 			$('div.road').addClass('inactive');
-// 			$('div.road').removeClass('active');
-			
-// 		}
-// 	});
-// })
-var count = 0;
-//since eq takes index as an argument, I probably need to set count equal to an index position?
+//Set counters for players 
+var player1 = 0;
+var player2 = 0;
 
 $(document).ready(function(){
 	$(document).on('keypress', function onKeypress(event){
 		
 		if(event.which === 97){
-			console.log("okay");
-			$('div').removeClass('active');
-			$('.road').eq(count + 1).addClass('active');
-			count++;
-			console.log(count);
+
+			$('div').removeClass('active1');
+			$('.player1 .road').eq(player1 + 1).addClass('active1');
+			player1++;
 		}
+
+		if(event.which === 108){
+
+			$('div').removeClass('active2');
+			$('.player2 .road').eq(player2 + 1).addClass('active2');
+			player2++;
+		}	
 	});
 })
 
