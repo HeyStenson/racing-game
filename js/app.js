9063da069d56f6e998140d44b6adb928e320c19e
HeyStenson <h.n.stenson@gmail.com>
2015-08-23 14:58:26 -0700

    omg it moooovvvveeees

--- js/app.js
+++ js/app.js
@@ -1,71 +1,64 @@
+var road1 = 0;
 var aKey = 97;
 var lKey = 108;
 var divs = 12;
+//var colorPicker = $('input');
 
-$(document).ready(function(){
 
-  //Player constructor.
-  function Player(player, color, key) {
-    this.player = player;
-    this.color = color;
-    this.key = key;
-    this.position = 0;
-    this.name = getName();
-    this.road = makeRoad(this.player);
-    this.move = move();
-  };
+// OOP Racing Game example boilerplate code
 
-  Player.prototype.getName = function(){
-    return prompt('What is your name?').toUpperCase();
-    //$('#name1').text(this.name);
-  };
+function Game() {
+  //Create a new instance of player 1
+  //this.player1 = new Player("Player 1");
 
-  Player.prototype.makeRoad = function(playerNum){
-    //create playerIdDiv
-    var playerIdDiv = $('div').addClass(playerNum);
+  //Do the same for a player 2
+  //this.player2 = new Player("Player 2");
+}
 
+// `Game.prototype.init` kicks off a new game with a board and two players
+Game.prototype.init = function() {
+  //
+};
+
+// A starter Player constructor.
+function Player(player) {
+  this.player = player;
+  // this.color = colorPicker.on('change', function(){
+  //   color = colorPicker.val();
+  // });
+  this.position = 0;
+  this.makeRoad = function(player){
+    $('.road-placeholder').addClass(player);
     for (var i = 0; i < divs; i++){
+      $('.road-placeholder').append('<div class="road"></div>');
       if (i === 0){
-        $('div').addClass('active1');
-        $(playerIdDiv).append('<div class="road"></div>');
-      } else {
-        $(playerIdDiv).append('<div class="road"></div>');
-      }
+        $('div.road').addClass('active1');
+      };
     }
-    //append playerIdDiv to body
-    $('body').append(playerIdDiv);
-  }
+  };
 
-  // Remember: prototypes are shared functions between all game instances
+  this.getName = function(){
+    var name = prompt('What is your name?').toUpperCase();
+    $('#name1').text(name);
+  };
+
+  this.move = $(document).on('keypress', function(event){
+    //if the key pressed is 'a'
+    if(event.which === aKey){
+      //remove the class active1 from the div, then add it to the next div with class of road in the player1 div
+      $('div.road').removeClass('active1');
+      $('.player1 .road').eq(road1 + 1).addClass('active1');
+      road1++;
+    }  
+  });
+};
+
+// Remember: prototypes are shared functions between all game instances
+Player.prototype.move = function() {
   //update player's position
-  Player.prototype.move = function() {
-    $(document).on('keypress', function(event){
-      //if the key pressed is the player's key
-      if(event.which === this.key){
-        //remove the class active1 from the div, then add it to the next div with class of road in the player's div
-        $('div').removeClass('active1');
-        $(playerIdDiv + '.road').eq(this.position + 1).addClass('active1');
-        player1++;
-      }
-    });  
-  };
+  //player++;
+};
 
-  function Game() {
-    //Create a new instance of player 1
-  var player1 = new Player("Player 1", "red");
-
-    //Do the same for a player 2
-  var player2 = new Player("Player 2", "blue");
-
-  }
-
-  // `Game.prototype.init` kicks off a new game with a board and two players
-  Game.prototype.init = function() {
-    //
-  };
-
-  // Start the game!
-  var game = new Game();
-  game.init();
-
-})
+// Start the game!
+var game = new Game();
+game.init();
